33621cbbb15e8c0c7e9c3b35f8e3180da4a2f301
grimhamr <rossiadisj@gmail.com>
2022-04-11 12:18:02 +0300

    run email local part check only if the local part contains a '.'

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,15 +1,19 @@
 class Invitation < ApplicationRecord
     validates :customer_email, presence: true
+    validates_format_of :customer_email, with: URI::MailTo::EMAIL_REGEXP
 
     before_validation :sanitize_email_field
-
+    
     private
       def sanitize_email_field
+        email_local_part = customer_email.split('@')[0]
+
         if customer_email.include?('<')
             self.display_name = customer_email.split('<')[0].chomp(' ')
             self.customer_email = customer_email.split('<')[1].chomp('>')
             
-        elsif customer_email.include?('.')
+        elsif email_local_part.include?('.')
+
             email_local_part = customer_email.split('@')[0]
             self.display_name = email_local_part.split('.')[0] + ' ' + email_local_part.split('.')[1]
         end
