dfbd97ac0c1c673ad30015c0df27d9a641e50427
grimhamr <rossiadisj@gmail.com>
2022-04-11 13:22:05 +0300

    added Invitation model tests

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'before validation' do  
+    it "parses email local parts with multiple dots" do
+      test_invitation = Invitation.create(customer_email: 'john.r.smith@gmail.com')
+      expect(test_invitation.display_name).to eql('john r smith')
+    end
+
+    it "parses email local parts with zero dots" do
+      test_invitation = Invitation.create(customer_email: 'johnsmith@gmail.com')
+      expect(test_invitation.display_name).to eql(nil)
+    end
+
+    it "parses name with email input" do
+      test_invitation = Invitation.create(customer_email: 'Jonathan Smith <john.smith@example.com>')
+      expect(test_invitation.display_name).to eql('Jonathan Smith')
+    end
+  end
 end
+
