b95ae1b7a2a1e13b15a77ed510c6f0ccbf1929ad
grimhamr <rossiadisj@gmail.com>
2022-04-12 21:55:52 +0300

    styling the modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_05_092136) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_081455) do
   create_table "cycles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_05_092136) do
     t.datetime "updated_at", null: false
     t.string "customer_email"
     t.text "message"
+    t.integer "cycle_id"
+    t.string "display_name"
+    t.index ["cycle_id"], name: "index_invitations_on_cycle_id"
   end
 
+  add_foreign_key "invitations", "cycles"
 end
